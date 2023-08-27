1ab3ceb00b33f210bc19148004e37e4f1c73ea28
Anup <anuppoudel60@gmail.com>
2023-08-27 13:34:36 +1000

    image optimization done

--- components/NewsArticleEntry.tsx
+++ components/NewsArticleEntry.tsx
@@ -1,5 +1,8 @@
 import { NewsArticle } from "@/models/NewsArticles";
 import { Card } from "react-bootstrap";
+import Image from "next/image";
+import placeholderImg from "@/assets/image/newarticle.png";
+import styles from "@/styles/NewsArticleEntry.module.css";
 
 interface NewsArticleEntryProps {
   article: NewsArticle;
@@ -15,7 +18,14 @@ const NewsArticleEntry = ({
   return (
     <a href={url}>
       <Card className="h-100">
-        <Card.Img variant="top" src={validImgUrl} />
+        {/* <Card.Img variant="top" src={validImgUrl} /> */}
+        <Image
+          src={validImgUrl || placeholderImg}
+          width={500}
+          height={200}
+          alt="News article Image"
+          className={`card-img-top ${styles.image}`}
+        />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{description}</Card.Text>
